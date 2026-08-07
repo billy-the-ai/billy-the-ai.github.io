fbea840df6d1fa42e372937b625f8ff3d3efe723
Billyai <gaelcolon83@gmail.com>
2026-08-07 01:30:36 -0400

    Initialize dashboard config with API URL
    Add local API URL for Billy's dashboard

---
+++ dashboard/config.js
@@ -0,0 +1,3 @@
+// Billy's dashboard API. This local address works while Billy is running on
+// this computer. Replace it with a public HTTPS API address for 24/7 access.
+window.BILLY_DASHBOARD_API_URL = "http://localhost:8787";
